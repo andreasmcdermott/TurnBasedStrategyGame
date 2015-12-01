817b3506bd602d933b9eefa7b1187ad5f885926d
Andreas McDermott <andreasmcdermott@gmail.com>
2015-12-01 02:43:11 +0000

    Added tests for assert and util.

---
+++ tst/testAssert.js
@@ -0,0 +1,60 @@
+var test = require('tape');
+var assert = require('../src/assert');
+
+test('is array', function (t) {
+  t.plan(7);
+  
+  t.doesNotThrow(function () { assert.isArray([]); });
+  t.throws(function () { assert.isArray(null); });
+  t.throws(function () { assert.isArray({}); });
+  t.throws(function () { assert.isArray(undefined); });
+  t.throws(function () { assert.isArray(''); });
+  t.throws(function () { assert.isArray('[]'); });
+  t.throws(function () { assert.isArray(0); });
+});
+
+test('is string', function (t) {
+  t.plan(6);
+  
+  t.doesNotThrow(function () { assert.isString(''); });
+  t.throws(function () { assert.isString([]); });
+  t.throws(function () { assert.isString({}); });
+  t.throws(function () { assert.isString(0); });
+  t.throws(function () { assert.isString(null); });
+  t.throws(function () { assert.isString(undefined); });
+});
+
+test('is number', function (t) {
+  t.plan(7);
+  
+  t.doesNotThrow(function () { assert.isNumber(0); });
+  t.throws(function () { assert.isNumber([]); });
+  t.throws(function () { assert.isNumber({}); });
+  t.throws(function () { assert.isNumber(''); });
+  t.throws(function () { assert.isNumber('0'); });
+  t.throws(function () { assert.isNumber(null); });
+  t.throws(function () { assert.isNumber(undefined); });
+});
+
+test('is object', function (t) {
+  t.plan(7);
+  
+  t.doesNotThrow(function () { assert.isObject({}); });
+  t.throws(function () { assert.isObject([]); });
+  t.throws(function () { assert.isObject(''); });
+  t.throws(function () { assert.isObject('{}'); });
+  t.throws(function () { assert.isObject(0); });
+  t.throws(function () { assert.isObject(null); });
+  t.throws(function () { assert.isObject(undefined); });
+});
+
+test('is not null or undefined', function (t) {
+  t.plan(6);
+  
+  t.doesNotThrow(function () { assert.isNotNullOrUndefined({}); });
+  t.doesNotThrow(function () { assert.isNotNullOrUndefined([]); });
+  t.doesNotThrow(function () { assert.isNotNullOrUndefined(''); });
+  t.doesNotThrow(function () { assert.isNotNullOrUndefined(0); });
+  t.throws(function () { assert.isNotNullOrUndefined(null); });
+  t.throws(function () { assert.isNotNullOrUndefined(undefined); });
+});
---
+++ tst/testUtil.js
@@ -0,0 +1,10 @@
+var test = require('tape');
+var util = require('../src/util');
+
+// test('expand with simple object', function (t) {
+  
+// });
+
+// test('copy array with simple array', function (t) {
+  
+// });
